fd4b2f9b4b5d6ac7418f42e4aedff24760c78954
joshuamccluskey <jpiff57@gmail.com>
2022-04-19 15:10:41 -0700

    Amplify models

---
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/ToolTypeEnum.java
@@ -0,0 +1,10 @@
+package com.amplifyframework.datastore.generated.model;
+/** Auto generated enum from GraphQL schema. */
+@SuppressWarnings("all")
+public enum ToolTypeEnum {
+  SLEDGEHAMMER,
+  DRILL,
+  CROWBAR,
+  JIGSAW,
+  CIRCULARSAW
+}
