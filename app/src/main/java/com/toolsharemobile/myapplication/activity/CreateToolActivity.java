54875c8b9a2c1586675dff2bc27d65c61449e8ff
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-04-20 15:46:44 -0700

    added location services api to determine user location

--- app/src/main/java/com/toolsharemobile/myapplication/activity/CreateToolActivity.java
+++ app/src/main/java/com/toolsharemobile/myapplication/activity/CreateToolActivity.java
@@ -29,7 +29,7 @@ public class CreateToolActivity extends AppCompatActivity {
 
     String TAG = " CREATE TOOL ACTIVITY";
     Spinner toolTypeSpinner = null;
-    FusedLocationProviderClient locationProvider = null;
+    FusedLocationProviderClient locationProviderClient = null;
     String lat = null;
     String lon = null;
 
@@ -37,11 +37,56 @@ public class CreateToolActivity extends AppCompatActivity {
     String username;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_tool);
+
+        lat = "90.0000";
+        lon = "45.0000";
+
+
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+        locationProviderClient.getLastLocation().addOnSuccessListener(location ->
+                {
+                    if (location == null) {
+                        Log.e(TAG, "Location callback was null!");
+                    }
+
+                    lat = Double.toString(location.getLatitude());
+                    lon = Double.toString(location.getLongitude());
+                    Log.i(TAG, "Our latitude: " + location.getLatitude());
+                    Log.i(TAG, "Our longitude: " + location.getLongitude());
+                }
+        ).addOnCanceledListener(() ->
+        {
+            Log.e(TAG, "Location request was canceled!");
+        })
+                .addOnFailureListener(failure ->
+                {
+                    Log.e(TAG, "Location request failed! Error was: " + failure.getMessage(), failure.getCause());
+                })
+                .addOnCompleteListener(complete ->
+                {
+                    Log.e(TAG, "Location request completed!");
+
+                });
 
 
         setUpCreateTool();
@@ -51,81 +96,22 @@ public class CreateToolActivity extends AppCompatActivity {
     }
 
 
-    public void setUpCreateTool(){
+    public void setUpCreateTool() {
 
-        if(Amplify.Auth.getCurrentUser() != null) {
+        if (Amplify.Auth.getCurrentUser() != null) {
             authUser = Amplify.Auth.getCurrentUser();
             username = authUser.getUsername();
         }
 
-
-
-
-
-
-
         LinearLayout addTaskButton = findViewById(R.id.buttonAddToolSubmit);
-        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        locationProvider = LocationServices.getFusedLocationProviderClient(getApplicationContext());
-        locationProvider.flushLocations();
-
-
-            lat = "90.0000";
-            lon = "45.0000";
-
-            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
-
-            {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                Log.e(TAG, "Application does not have access to either ACCESS_FINE_LOCATION or ACCESS_COARSE_LOCATION!");
-                return;
-            }
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            locationProvider.getLastLocation().addOnSuccessListener(location ->  // "location" here could be null if no one else has request a location prior!
-                            // Try running Google Maps first if you have a null callback here!
-                    {
-                        if (location == null)
-                        {
-                            Log.e(TAG, "Location callback was null!");
-                        }
-                        lat = Double.toString(location.getLatitude());
-                        lon = Double.toString(location.getLongitude());
-                        Log.i(TAG, "Our latitude: " + location.getLatitude());
-                        Log.i(TAG, "Our longitude: " + location.getLongitude());
-                    }
-            ).addOnCanceledListener(() ->
-            {
-                Log.e(TAG, "Location request was canceled!");
-            })
-                    .addOnFailureListener(failure ->
-                    {
-                        Log.e(TAG, "Location request failed! Error was: " + failure.getMessage(), failure.getCause());
-                    })
-                    .addOnCompleteListener(complete ->
-                    {
-                        Log.e(TAG, "Location request completed!");
-
-                    });
-
-
         addTaskButton.setOnClickListener(v -> {
 
 
             Tool newTool = Tool.builder()
-                    .toolType((ToolTypeEnum)toolTypeSpinner.getSelectedItem())
+                    .toolType((ToolTypeEnum) toolTypeSpinner.getSelectedItem())
 
                     .listedByUser(username)
                     .location(lat + "," + lon)
-
-                    
-
-
                     .isAvailable(true)
                     .openReturnRequest(false)
                     .openBorrowRequest(false)
@@ -157,14 +143,6 @@ public class CreateToolActivity extends AppCompatActivity {
 
         toolTypeSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.preference_category, ToolTypeEnum.values()));
     }
-
-
-
-
-
-
-
-
 
 
 }
