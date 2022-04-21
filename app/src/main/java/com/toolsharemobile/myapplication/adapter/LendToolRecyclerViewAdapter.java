3010f5b8b1c176277d38e7e0053b4d99ac033aa0
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-04-21 13:48:21 -0700

    added filter by closest location; fixed ui bugs in find a tool listing

--- app/src/main/java/com/toolsharemobile/myapplication/adapter/LendToolRecyclerViewAdapter.java
+++ app/src/main/java/com/toolsharemobile/myapplication/adapter/LendToolRecyclerViewAdapter.java
@@ -20,8 +20,7 @@ import com.toolsharemobile.myapplication.activity.ViewLendedToolActivity;
 
 import java.util.List;
 
-public class LendToolRecyclerViewAdapter extends RecyclerView.Adapter{
-
+public class LendToolRecyclerViewAdapter extends RecyclerView.Adapter {
 
 
     List<Tool> toolList;
@@ -29,8 +28,7 @@ public class LendToolRecyclerViewAdapter extends RecyclerView.Adapter{
     public static final String TAG = "Tool to delete";
 
 
-
-    public LendToolRecyclerViewAdapter(List<Tool> toolList, Context callingActivity){
+    public LendToolRecyclerViewAdapter(List<Tool> toolList, Context callingActivity) {
         this.toolList = toolList;
         this.callingActivity = callingActivity;
 
@@ -50,20 +48,25 @@ public class LendToolRecyclerViewAdapter extends RecyclerView.Adapter{
         TextView pendingTextView = holder.itemView.findViewById(R.id.pendingTextView);
         ImageView imageViewToolIcon = holder.itemView.findViewById(R.id.imageViewToolIcon);
 
-        if (toolList.get(position).getToolType().equals(ToolTypeEnum.CROWBAR)) imageViewToolIcon.setImageResource(R.drawable.crowbar);
-        if (toolList.get(position).getToolType().equals(ToolTypeEnum.SLEDGEHAMMER)) imageViewToolIcon.setImageResource(R.drawable.sledgehammer);
-        if (toolList.get(position).getToolType().equals(ToolTypeEnum.DRILL)) imageViewToolIcon.setImageResource(R.drawable.drill);
-        if (toolList.get(position).getToolType().equals(ToolTypeEnum.CIRCULARSAW)) imageViewToolIcon.setImageResource(R.drawable.circular_saw);
-        if (toolList.get(position).getToolType().equals(ToolTypeEnum.JIGSAW)) imageViewToolIcon.setImageResource(R.drawable.jigsaw);
+        if (toolList.get(position).getToolType().equals(ToolTypeEnum.CROWBAR))
+            imageViewToolIcon.setImageResource(R.drawable.crowbar);
+        if (toolList.get(position).getToolType().equals(ToolTypeEnum.SLEDGEHAMMER))
+            imageViewToolIcon.setImageResource(R.drawable.sledgehammer);
+        if (toolList.get(position).getToolType().equals(ToolTypeEnum.DRILL))
+            imageViewToolIcon.setImageResource(R.drawable.drill);
+        if (toolList.get(position).getToolType().equals(ToolTypeEnum.CIRCULARSAW))
+            imageViewToolIcon.setImageResource(R.drawable.circular_saw);
+        if (toolList.get(position).getToolType().equals(ToolTypeEnum.JIGSAW))
+            imageViewToolIcon.setImageResource(R.drawable.jigsaw);
 
 
         textViewToolName.setText(toolList.get(position).getToolType().toString());
         Tool tool = toolList.get(position);
 
-            pendingTextView.setVisibility(View.INVISIBLE);
-            if (tool.getOpenBorrowRequest() == true || tool.getOpenReturnRequest() == true) {
-                pendingTextView.setVisibility(View.VISIBLE);
-            }
+        pendingTextView.setVisibility(View.INVISIBLE);
+        if (tool.getOpenBorrowRequest() == true || tool.getOpenReturnRequest() == true) {
+            pendingTextView.setVisibility(View.VISIBLE);
+        }
 
         View toolViewHolder = holder.itemView;
         toolViewHolder.setOnClickListener(view -> {
@@ -79,9 +82,9 @@ public class LendToolRecyclerViewAdapter extends RecyclerView.Adapter{
         return toolList.size();
     }
 
-    public static class ToolListViewHolder extends RecyclerView.ViewHolder{
+    public static class ToolListViewHolder extends RecyclerView.ViewHolder {
 
-        public ToolListViewHolder(View itemView){
+        public ToolListViewHolder(View itemView) {
             super(itemView);
         }
     }
